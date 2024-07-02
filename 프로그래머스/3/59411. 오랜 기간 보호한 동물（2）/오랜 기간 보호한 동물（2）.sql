c6337abbc435b57c917666160693811a82feac40
YONG HYEOK LEE <77919992+LEE-26@users.noreply.github.com>
2024-07-02 15:54:49 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+# 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회
+
+# SELECT 
+#     I.ANIMAL_ID,
+#     I.NAME,
+#     DATEDIFF(O.DATETIME - I.DATETIME) DIFF_DATE
+# FROM ANIMAL_INS I LEFT JOIN ANIMAL_OUTS O USING (ANIMAL_ID)
+                                                 
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O USING (ANIMAL_ID)
+ORDER BY TIMESTAMPDIFF(DAY, I.DATETIME, O.DATETIME) DESC
+LIMIT 2;
