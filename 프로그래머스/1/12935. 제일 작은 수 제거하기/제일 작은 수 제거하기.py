d302efbe2d461f46d75eae91aa40fc734a299125
YONG HYEOK LEE <77919992+LEE-26@users.noreply.github.com>
2024-07-05 17:43:27 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,11 +1,11 @@
-# def solution(arr):
-#     arr.sort(reverse = True)
-#     arr_pop = arr.pop()
-#     if len(arr) < 1:
-#         return [-1]
-#     else : 
-#         return arr
-
+def solution(arr):
+    if len(arr) > 1:    
+        arr.sort(reverse = True)
+        arr_pop = arr.pop(-1)
+        return arr
+    else : 
+        return [-1]
+    
 def solution(arr):
     if len(arr) > 1:
         arr.remove(min(arr))
